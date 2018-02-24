7dc1eabe2c3988e8506b809d88ed914e9b5cd1a1
mrcrmn <marcoreimann@outlook.de>
2018-02-24 15:09:33 +0100

    docblocks

--- src/Database/Connectors/DatabaseConnection.php
+++ src/Database/Connectors/DatabaseConnection.php
@@ -16,23 +16,66 @@ namespace Backbone\Database\Connectors;
 */
 class DatabaseConnection extends \PDO
 {
+    /**
+     * The database driver.
+     *
+     * @var string
+     */
     protected $driver = "mysql";
 
+    /**
+     * The resolved DNS.
+     *
+     * @var string
+     */
     protected $dns;
 
+    /**
+     * The username.
+     *
+     * @var string
+     */
     protected $username;
 
+    /**
+     * The password.
+     *
+     * @var string
+     */
     protected $password;
 
+    /**
+     * The database name
+     *
+     * @var string
+     */
     protected $database;
-    
-    public function __construct(string $host, string $username, string $password, int $port, string $database)
+
+    /**
+     * Creates a new PDO Instance.
+     *
+     * @param string $host     The hostname.
+     * @param string $username The Username.
+     * @param string $password The Password.
+     * @param int    $port     The Port.
+     * @param string $database The Database name.
+     */
+    public function __construct($host, $username, $password, $port, $database)
     {
         $this->dns = $this->resolveDns($host, $port, $database);
 
         parent::__construct($this->dns, $username, $password);
     }
 
+    /**
+     * Resolves the DNS part for the PDO connection.
+     *
+     * @param  string $host     The hostname.
+     * @param  int $port     The port.
+     * @param  string $database The database name.
+     *
+     * @return string
+     */
     protected function resolveDns($host, $port, $database)
     {
         return sprintf("%s:dbname=%s;host=%s;port=%d", $this->driver, $database, $host, $port);
